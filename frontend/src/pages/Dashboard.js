a4d397f4a18a0225b3abfef1972613becf3b4931
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-06 01:11:04 +0530

    add a summary component

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -16,6 +16,7 @@ const Dashboard = () => {
     return (
         <>
             <Navbar updateState={updateState} currState={display}/>
+            {display === "summary" && <Summary/>}
             {display === "newTest" && <Test/>}
         </>
     )
