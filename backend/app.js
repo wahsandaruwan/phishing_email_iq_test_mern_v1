8394d81ead54bbddebcf5d87026e693f9b27a96f
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-14 06:13:00 +0530

    code updated

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const app = express()
 const PORT = 3300
 require('./helpers/mongodb_init')
 
+// Basic route
 app.get('/', (req, res) => {
     res.send("Welcome!")
 })
 
+// Bind and listen the connection on host and port
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
 })
