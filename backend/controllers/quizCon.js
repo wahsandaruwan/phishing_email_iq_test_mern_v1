e75017bfcf7e66fc7507d3da5517b501e2224c41
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-15 00:20:14 +0530

    Updated quiz controller added

--- backend/controllers/quizCon.js
+++ backend/controllers/quizCon.js
@@ -23,8 +23,8 @@ exports.addQuiz = async (req, res) => {
     // Create new quiz
     const newQuiz = new Quiz({title, quizImage, quizAns})
     try{
-        const quiz = await newQuiz.save()
-        res.status(200).json(quiz)
+        await newQuiz.save()
+        res.status(200).json({success: {message: "Quiz successfully created!"}})
     }catch(err){
         res.status(403).json({errors: {message: Object.entries(err.errors)[0][1].message}})
     }
@@ -42,17 +42,31 @@ exports.getQuizById = async (req, res) => {
     }
 }
 
-// Get quizes by searching
-exports.getQuizBySearch = async (req, res) => {
-    
-}
-
 // Update quiz
 exports.updateQuiz = async (req, res) => {
-    
+    const {quizId} = req.params
+
+    // Check if quiz title already exist
+    const quiz = await Quiz.findOne({title: req.body.title})
+
+    if(quiz){
+        return res.status(403).json({error: {message: "Quiz title already exist!"}})
+    }
+
+    try{
+        await Quiz.findByIdAndUpdate(quizId, req.body)
+        res.status(200).json({success: {message: "Quiz uccessfully updated!"}})
+    }catch(err){
+        res.status(403).json({errors: {message: Object.entries(err.errors)[0][1].message}})
+    }
 }
 
 // Delete quiz
 exports.deleteQuiz = async (req, res) => {
+    
+}
+
+// Get quizes by searching
+exports.getQuizBySearch = async (req, res) => {
     
 }
