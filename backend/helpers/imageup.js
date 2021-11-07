f1846068417fce3302c8b85d378dca04a78d15f6
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-07 21:49:56 +0530

    backend : Added file filtering for image uploading

--- backend/helpers/imageup.js
+++ backend/helpers/imageup.js
@@ -10,6 +10,16 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = (req, file, callback) => {
+    if(file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg"){
+        callback(null, true)
+    }
+    else{
+        callback(null, false)
+    }
+}
+
 exports.upload = multer({
-    storage: storage
+    storage: storage,
+    fileFilter: fileFilter
 }).single("quizImage")
