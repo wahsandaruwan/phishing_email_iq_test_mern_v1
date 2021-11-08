8662a742664be56859928286adb9cb7169d55df3
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-09 04:39:59 +0530

    frontend: Welcome user name added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = ({ updateState, currState }) => {
 
     // User data in local storage
     const userData = localStorage.getItem('userWithToken')
+    const { firstName, userType } = JSON.parse(userData).userInfo
 
     // Logout handler
     const logoutHandler = (e) => {
@@ -16,35 +17,32 @@ const Navbar = ({ updateState, currState }) => {
 
     // Elements only for normal users
     const displayOnUserType = () => {
-        if(userData){
-            const { userType } = JSON.parse(userData).userInfo
-            if(userType === "normal"){
-                return (
-                    <>
-                        <li>
-                        <a className={`${currState === "myAllTest" ? "active" : ""}`} onClick={() => updateState("myAllTest")}>My All Tests</a>
-                        </li>
-                        <li>
-                            <a className={`${currState === "newTest" ? "active" : ""}`} onClick={() => updateState("newTest")}>New Test</a>
-                        </li>
-                    </>
-                )
-            }
-            else{
-                return (
-                    <>
-                        <li>
-                        <a className={`${currState === "allTest" ? "active" : ""}`} onClick={() => updateState("allTest")}>All Tests</a>
-                        </li>
-                        <li>
-                            <a className={`${currState === "allUsers" ? "active" : ""}`} onClick={() => updateState("allUsers")}>All Users</a>
-                        </li>
-                        <li>
-                            <a className={`${currState === "allQuizes" ? "active" : ""}`} onClick={() => updateState("allQuizes")}>All Quizes</a>
-                        </li>
-                    </>
-                )
-            }
+        if(userType === "normal"){
+            return (
+                <>
+                    <li>
+                    <a className={`${currState === "myAllTest" ? "active" : ""}`} onClick={() => updateState("myAllTest")}>My All Tests</a>
+                    </li>
+                    <li>
+                        <a className={`${currState === "newTest" ? "active" : ""}`} onClick={() => updateState("newTest")}>New Test</a>
+                    </li>
+                </>
+            )
+        }
+        else{
+            return (
+                <>
+                    <li>
+                    <a className={`${currState === "allTest" ? "active" : ""}`} onClick={() => updateState("allTest")}>All Tests</a>
+                    </li>
+                    <li>
+                        <a className={`${currState === "allUsers" ? "active" : ""}`} onClick={() => updateState("allUsers")}>All Users</a>
+                    </li>
+                    <li>
+                        <a className={`${currState === "allQuizes" ? "active" : ""}`} onClick={() => updateState("allQuizes")}>All Quizes</a>
+                    </li>
+                </>
+            )
         }
     }
 
@@ -58,6 +56,9 @@ const Navbar = ({ updateState, currState }) => {
                     {displayOnUserType()}
                     <li>
                         <a href="#" onClick={(e) => logoutHandler(e)} className="lgo-btn">Logout</a>
+                    </li>
+                    <li style={{backgroundColor: "#fff", color: "#000", padding: "10px 25px", borderRadius: "8px"}}>
+                        Welcome {firstName}
                     </li>
                 </ul>
             </section>
