ce6b9c8f0f0023311cf1da657872281429fee9b8
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-11 18:40:10 +0530

    code updated

--- frontend/src/components/QuizStart.js
+++ frontend/src/components/QuizStart.js
@@ -5,6 +5,7 @@ const QuizStart = ({onQuizStart}) => {
     return (
         <>
             <div className="quiz-start-end">
+                <h3>Phishing IQ Test</h3>
                 <div>
                     <SubmitBtn clickFunc={onQuizStart} txt="Start the Quiz"/>
                 </div>
