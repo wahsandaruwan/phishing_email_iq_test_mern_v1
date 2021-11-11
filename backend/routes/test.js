be2c7a0439bc5664e85518d0621272d235a77165
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-12 03:20:34 +0530

    code updated

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -6,10 +6,10 @@ const {authRole} = require('../middlewares/auth')
 // Get all quizes router
 router.get('/', authRole(['admin', 'normal']), getAllTests)
 
-// Add quiz router
+// Add tests router
 router.post('/', authRole(['normal']), addTest)
 
-// Add quiz router
+// Get all tests by email router
 router.get('/:userEmail', authRole(['normal']), getTestsByEmail)
 
 module.exports = router
--- frontend/src/data/tempTbl.js
+++
@@ -1,8 +0,0 @@
-// Temp user test table
-export const TblData = [
-    {Tid: "ti1", Time: "22:30:25", Duration : "5 min. 30 sec.", Date: "2021-10-04", Marks: 9, Recommendations: "Great Job!"},
-    {Tid: "ti2", Time: "14:20:10", Duration : "7 min. 40 sec.", Date: "2021-10-03", Marks: 8, Recommendations: "Great Job!"},
-    {Tid: "ti3", Time: "10:05:10", Duration : "4 min. 39 sec.", Date: "2021-10-03", Marks: 7, Recommendations: "Good Job!"},
-    {Tid: "ti4", Time: "12:55:22", Duration : "8 min. 12 sec.", Date: "2021-10-02", Marks: 8, Recommendations: "Great Job!"},
-    {Tid: "ti5", Time: "13:33:43", Duration : "5 min. 10 sec.", Date: "2021-10-01", Marks: 5, Recommendations: "Try Again!"}
-]
