6ed411508bdd5cd52d4b70b53849c9b4e542a9aa
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-11 21:05:40 +0530

    frontend & backend: show all tests to current user

--- frontend/src/components/TableUserAllTests.js
+++ frontend/src/components/TableUserAllTests.js
@@ -1,44 +1,90 @@
-// Data
-import { useState } from 'react'
-import { TblData } from '../data/tempTbl'
+import { useState, useEffect } from 'react'
+import { useHistory } from "react-router-dom"
+import {formatTime} from '../utils/timeformat'
+import axios from 'axios'
 
 
 const TableUserAllTests = () => {
-    // User test table
-    const [data] = useState(TblData)
+    // Tests state
+    const [tests, setTests] = useState([])
+
+    // Test count
+    let num = 0;
+
+    // Set history
+    const history = useHistory()
+
+    // Token from local storage
+    const userData = localStorage.getItem('userWithToken')
+    const user = JSON.parse(userData).userInfo
+    const token = JSON.parse(userData).success
+
+    // Create config with token
+    const configCommon = {
+        headers: { "Authorization": `Bearer ${token}` }
+    }
+
+    // Fetching tests handler
+    const testFetchhandler = async () => {
+        try {
+            const {data} = await axios.get(
+                `http://localhost:3300/api/tests/`,
+                configCommon
+            )
+            if(data.authEx){
+                alert(data.errors.message)
+                // Clear local storage and navigate to login page
+                localStorage.clear()
+                history.push("/")
+            }
+            else{
+                setTests(data)
+            }
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
+    // Handle fetching all users
+    useEffect(() => {
+        testFetchhandler()
+    }, [])
+
     return (
         <>
             <section className="all-data">
-                <h3>All Past Tests</h3>
-                {data.length > 0 && (
+                <h3>My Past Tests</h3>
+                {tests.length > 0 && (
                     <div className="tbl-div">
                         <table className="dt-table">
                             <thead>
                                 <tr>
-                                    {
-                                        Object.keys(data[0]).map((heading, index) => {
-                                            return(
-                                                <th key={index}>{heading}</th>
-                                            )
-                                        })
-                                    }
+                                    <th>#</th>
+                                    <th>Creation Date</th>
+                                    <th>Creation Time</th>
+                                    <th>Time Duration</th>
+                                    <th>Marks</th>
+                                    <th>Recommendation</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
-                                    data.map((obj) => {
+                                    tests.map((obj) => {
                                         // Destructure
-                                        const {Tid, Time, Duration, Date, Marks, Recommendations} = obj
-                                        return(
-                                            <tr key={Tid}>
-                                                <td>{Tid}</td>
-                                                <td>{Time}</td>
-                                                <td>{Duration}</td>
-                                                <td>{Date}</td>
-                                                <td>{Marks}</td>
-                                                <td>{Recommendations}</td>
-                                            </tr>
-                                        )
+                                        const {_id, userEmail, currentDate, currentTime, timeDuration, correct, recommendation} = obj
+                                        if(userEmail === user.email){
+                                            num++
+                                            return(
+                                                <tr key={_id}>
+                                                    <td>{num}</td>
+                                                    <td>{currentDate}</td>
+                                                    <td>{currentTime}</td>
+                                                    <td>{formatTime(timeDuration)}</td>
+                                                    <td>{correct}</td>
+                                                    <td>{recommendation}</td>
+                                                </tr>
+                                            )
+                                        }
                                     })
                                 }
                             </tbody>
